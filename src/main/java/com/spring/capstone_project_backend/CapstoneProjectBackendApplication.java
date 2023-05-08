f60d0358e32e30c2adb9410f0b4262b299990f03
SamueleRusco <119084990+SamueleRusco@users.noreply.github.com>
2023-05-08 12:01:49 +0200

    Initial commit

---
+++ src/main/java/com/spring/capstone_project_backend/CapstoneProjectBackendApplication.java
@@ -0,0 +1,15 @@
+package com.spring.capstone_project_backend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CapstoneProjectBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CapstoneProjectBackendApplication.class, args);
+		
+		System.out.println("application ok...");
+	}
+
+}
---
+++ src/test/java/com/spring/capstone_project_backend/CapstoneProjectBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.spring.capstone_project_backend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CapstoneProjectBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
